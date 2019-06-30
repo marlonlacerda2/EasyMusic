ed7c99beab744301c0cecef2b2871fb1eab033ad
Ana-Dias-ACD <anadiasacd690@gmail.com>
2019-06-29 21:58:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.myapplication.R.id.jogar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button jogar= findViewById(R.id.jogar);
+        Button sair= findViewById(R.id.sair);
+        jogar.setOnClickListener(this);
+        sair.setOnClickListener(this);
+    }
+public void onClick(View v){
+
+    switch (v.getId()){
+        case R.id.sair:
+            this.finish();
+            break;
+
+    }
+}
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
